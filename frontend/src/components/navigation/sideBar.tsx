23d1d454f5ff673a1c8b7832f6a4892b1a1e5e87
daniel <wazwazman1@gmail.com>
2023-11-16 00:39:26 +0200

    user can now accses login screen

--- frontend/src/components/navigation/sideBar.tsx
+++ frontend/src/components/navigation/sideBar.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import Link from "next/link";
-import { DevicesOther, AccessTime, Settings, Logout } from '@mui/icons-material';
+import { DevicesOther, AccessTime, Settings, Logout, Login } from '@mui/icons-material';
 import { useAuth } from "@/hooks/useAuth";
+import { useRouter } from "next/navigation";
 
 
 export default function SideBar() {
+    const router = useRouter()
     const {userdata, login, logout,signup, updateUserData, isError, error} = useAuth();
     return (
         <div className="fixed flex flex-wrap content-between top-11 left-0 bg-surface w-11 h-[calc(100%-44px)] md:w-24 md:h-[calc(100%-48px)] xl:top-12">
@@ -23,7 +25,7 @@ export default function SideBar() {
             <div onClick={() => {}} className="w-11 h-11 md:w-24 md:h-24 flex justify-center items-center cursor-pointer duration-100 hover:scale-105">
                 {userdata.token != "" ? 
                 <Logout className="fill-white w-3/4 h-3/4" onClick={() => {logout()}}/> : 
-                <></>
+                <Login className="fill-white w-3/4 h-3/4" onClick={() => {router.push("/login")}}/>
                 }
             </div>
         </div>
