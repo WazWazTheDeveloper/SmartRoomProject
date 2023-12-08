432f982e52a93dbcb5df9a4b10a342cd68ab1368
daniel <wazwazman1@gmail.com>
2023-12-08 09:33:23 +0200

    added permission Group settings page

---
+++ frontend/src/components/options/permissionGroups/PermissionGroupsSettingsPage.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useAppdata } from "@/hooks/useAppdata";
+import { useAuth } from "@/hooks/useAuth";
+import { DeleteForever, Settings } from "@mui/icons-material";
+import Error from "next/error";
+import { useRouter } from "next/navigation";
+
+export function PermissionGroupsSettingsPage(props: any) {
+    const router = useRouter()
+
+    const [appdata, isAppdata] = useAppdata();
+    const { userdata } = useAuth();
+
+    function redirectToUserSettingsPage(id: string) {
+        router.push(`/settings/user/${id}`)
+    }
+
+    if (userdata && !userdata.isAdmin) {
+        return (
+            <Error statusCode={401} />
+        )
+    }
+
+    return (
+        <div className="w-full md:w-3/5 text-on-surface bg-surface">
+            <p className="underline">Permission Groups:</p>
+            {/* TODO: get group list from appdata i think? */}
+            {/* {isAppdata ? appdata.getUserList().map((user, index) =>
+                <div key={index} className="relative w-full flex h-9 flex-nowrap justify-start items-center overflow-hidden md:h-9">
+                    <div className="relative w-9/12 md:w-[70%] ml-2 flex item-center">
+                        <p className="w-full overflow-ellipsis overflow-hidden whitespace-nowrap">{user.username}</p>
+                    </div>
+                    <div className="flex justify-center w-3/12 relative md:w-[30%] md:h-full md:justify-end items-center">
+                        <Settings className="fill-on-surface ml-1 cursor-pointer" onClick={() => { redirectToUserSettingsPage(user.uuid) }} />
+                        <DeleteForever className="fill-on-surface ml-1 cursor-pointer" onClick={() => { }} />
+                    </div>
+                </div>
+            ) : <></>} */}
+        </div>
+    )
+
+}
