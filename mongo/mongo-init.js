a92af7380e87e6c28e25d0ee0a3201786ee010be
daniel <wazwazman1@gmail.com>
2024-02-06 23:07:18 +0200

    docker compose fixes

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -2,8 +2,8 @@
 rs.initiate({
     _id: "rs0",
     members: [
-        { _id: 0, host: "mongo01" },
-        { _id: 1, host: "mongo02" },
-        { _id: 2, host: "mongo03" }
+        { _id: 0, host: "mongo1" },
+        { _id: 1, host: "mongo2" },
+        { _id: 2, host: "mongo3" }
     ]
 })
