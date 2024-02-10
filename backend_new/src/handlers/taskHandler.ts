262291d6cc219d2a38272f9b7346c2fdcf46c93d
daniel <wazwazman1@gmail.com>
2024-02-11 00:23:01 +0200

    moved some code + added boilerplate code

--- backend_new/src/handlers/taskHandler.ts
+++ backend_new/src/handlers/taskHandler.ts
@@ -1,3 +1,10 @@
+
+// check and execute task
 export function taskCheckHandler(taskID: string) {
+
+}
+
+// handler for taskSchedulerService to update db timeCheck isTrue property
+export function taskTimeCheckHandler(taskID: string) {
 
 }
