3c5ad385ea05d8bd67e06c97fe6fe5894ca436e8
WazWaz <wazwazman1@gmail.com>
2024-02-22 15:40:01 +0200

    finished mqttMassge.interface.ts

--- backend_new/src/interfaces/mqttMassge.interface.ts
+++ backend_new/src/interfaces/mqttMassge.interface.ts
@@ -1,23 +1,53 @@
 import { DeviceDataTypesConfigs, TDeviceDeviceDataPropertiesAny } from "./deviceData.interface"
 
-type TInitDeviceMassage = {
+type TInitDeviceRequest = {
+    operation : "initDevice"
     origin : string
     dataTypeArray: DeviceDataTypesConfigs[]
     deviceName?: string,
 }
 
 type TInitDeviceRespone = {
+    operation : "initDevice"
     target : string,
     isSuccessful : false
 } | {
+    operation : "initDevice"
     target : string,
     isSuccessful : true,
     _id : string
 }
 
+type TGetDeviceRequest = {
+    operation : "getDevice"
+    deviceID : string
+}
 
-type TUpdateDataMassage = {
+type TUpdateDataFromDeviceRequest = {
+    deviceID : string
+    operation : "updateServer"
+    dataID : number
     typeID : number
 } & TDeviceDeviceDataPropertiesAny
 
-// add respond expected type
+type TUpdateDataFromDeviceResponse = {
+    isSuccessful : boolean
+}
+
+type TUpdateDataToDeviceRequest = {
+    operation : "updateDevice"
+    typeID : number
+} & TDeviceDeviceDataPropertiesAny
+
+type TUpdateDataToDeviceResponse = {
+    isSuccessful : boolean
+}
+
+type TConnectionCheckRequest = {
+    operation : "checkConnection"
+}
+
+type TConnectionCheckResponse = {
+    operation : "checkConnection"
+    deviceID : string
+}
