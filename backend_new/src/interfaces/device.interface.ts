839d876ac9f2aa8ea849461f6b2bcee82ea3ec16
WazWaz <wazwazman1@gmail.com>
2024-02-26 22:29:15 +0200

    code changes

--- backend_new/src/interfaces/device.interface.ts
+++ backend_new/src/interfaces/device.interface.ts
@@ -1,51 +1,66 @@
-import MultiStateButton from "../models/dataTypes/multiStateButtonData"
-import NumberData from "../models/dataTypes/numberData"
-import SwitchData from "../models/dataTypes/switchData"
-import { DeviceDataTypes, DeviceDataTypesConfigs, TDeviceDataProperties } from "./deviceData.interface"
+import MultiStateButton from "../models/dataTypes/multiStateButtonData";
+import NumberData from "../models/dataTypes/numberData";
+import SwitchData from "../models/dataTypes/switchData";
+import {
+    DeviceDataTypes,
+    DeviceDataTypesConfigs,
+    TDeviceDataProperties,
+} from "./deviceData.interface";
 
 export type TDeviceProperty =
-    {
-        propertyName: "deviceName"
-        newValue: string
-    } |
-    {
-        propertyName: "mqttTopicID"
-        newValue: string
-    } |
-    {
-        propertyName: "isAccepted"
-        newValue: -1 | 0 | 1
-    } |
-    {
-        propertyName: "isAdminOnly"
-        newValue: boolean
-    } |
-    ({
-        propertyName: "data"
-    } & TDeviceDataProperties)
+    | {
+          propertyName: "deviceName";
+          newValue: string;
+      }
+    | {
+          propertyName: "mqttTopicID";
+          newValue: string;
+      }
+    | {
+          propertyName: "isAccepted";
+          newValue: -1 | 0 | 1;
+      }
+    | {
+          propertyName: "isAdminOnly";
+          newValue: boolean;
+      }
+    | ({
+          propertyName: "data";
+      } & TDeviceDataProperties)
+    | {
+          propertyName: "isConnected";
+          newValue: boolean;
+      }
+    | {
+          propertyName: "isConnectedCheck";
+          newValue: boolean;
+      };
 
 export type TDevice = {
-    _id: string
-    deviceName: string
-    mqttTopicID: string
-    isAccepted: -1 | 0 | 1
-    isAdminOnly: boolean
-    data: (TDeviceDataObject)[]
-}
+    _id: string;
+    deviceName: string;
+    mqttTopicID: string;
+    isAccepted: -1 | 0 | 1;
+    isAdminOnly: boolean;
+    data: TDeviceDataObject[];
+    isConnected: boolean;
+    isConnectedCheck: boolean;
+};
 
 export type TDeviceJSON_DB = {
-    _id: string
-    deviceName: string
-    mqttTopicID: string
-    isAccepted: -1 | 0 | 1
-    isAdminOnly: boolean
-    data: DeviceDataTypes[]
-}
+    _id: string;
+    deviceName: string;
+    mqttTopicID: string;
+    isAccepted: -1 | 0 | 1;
+    isAdminOnly: boolean;
+    data: DeviceDataTypes[];
+    isConnected: boolean;
+    isConnectedCheck: boolean;
+};
 
 export type TDataConfig = {
-    typeID: number
-    dataConfig: DeviceDataTypesConfigs
-}
+    typeID: number;
+    dataConfig: DeviceDataTypesConfigs;
+};
 
 export type TDeviceDataObject = SwitchData | NumberData | MultiStateButton;
-
