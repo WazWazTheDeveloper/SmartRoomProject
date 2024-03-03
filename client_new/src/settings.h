da3e4fa49a1d3d0f52bfc49b195fbe7ad74295f3
WazWaz <wazwazman1@gmail.com>
2024-03-03 19:41:08 +0200

    code changes

---
+++ client_new/src/settings.h
@@ -0,0 +1,11 @@
+#include "pinSettings.cpp"
+
+// WIFI
+char *wifi_ssid = ("home");            // wifi ssid
+char *wifi_password = ("0525611397");  // wifi password
+
+// MQTT broker
+const char brokerIp[] = ("10.0.0.12"); // mqtt broker ip
+int brokerPort = 1883; // mqtt broker port
+char *connectionCheckRequestTopic = ("checkConnectionRequest");
+char *connectionCheckResponseTopic = ("checkConnectionResponse");
