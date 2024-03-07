e29f6f41b23452b513295d85ad5557e455579796
WazWaz <wazwazman1@gmail.com>
2024-03-07 20:07:25 +0200

    new client code

--- client_new/src/settings.h
+++ client_new/src/settings.h
@@ -9,3 +9,8 @@ const char brokerIp[] = ("10.0.0.12"); // mqtt broker ip
 int brokerPort = 1883; // mqtt broker port
 char *connectionCheckRequestTopic = ("checkConnectionRequest");
 char *connectionCheckResponseTopic = ("checkConnectionResponse");
+
+
+void deviceSetup() {
+    
+}
