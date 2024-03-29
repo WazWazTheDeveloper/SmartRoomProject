e4dc1d7a0efceb5eda04e2222b62736eda948f26
daniel <wazwazman1@gmail.com>
2024-03-30 01:29:52 +0300

    add selfsigned certificate to use https in development

--- auth/app.ts
+++ auth/app.ts
@@ -1,10 +1,18 @@
 import express = require('express');
+import https from "https";
 const cookieParser = require('cookie-parser')
 import bodyParser = require('body-parser');
 import { routerv1 } from './src/routes/v1/router';
 import { connectToDatabase } from './src/services/mongoDBService';
+import fs from 'fs';
 
 const app = express();
+var key = fs.readFileSync('./certs/selfsigned.key');
+var cert = fs.readFileSync('./certs/selfsigned.crt');
+var options = {
+    key: key,
+    cert: cert
+};
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -17,7 +25,8 @@ async function startServer(): Promise<void> {
 }
 
 function startListeningToReqests(): void {
-    let server = app.listen(process.env.SERVER_PORT, () => {
+    let server = https.createServer(options, app);
+    server.listen(process.env.SERVER_PORT, () => {
         // TODO: log this:
         console.log(`listening on port ${process.env.SERVER_PORT}`)
     })
