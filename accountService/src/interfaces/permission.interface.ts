764b2be747067d3f6c365c5a365b2acc671e9af6
daniel <wazwazman1@gmail.com>
2024-04-04 00:14:10 +0300

    edited interface

--- accountService/src/interfaces/permission.interface.ts
+++ accountService/src/interfaces/permission.interface.ts
@@ -1,6 +1,6 @@
 export type TPermission = {
     type: "topic" | "device" | "task" | "PermissionGroup" | "users"
-    objectId: string
+    objectId: string | "all"
     read: boolean
     write: boolean
     delete: boolean
