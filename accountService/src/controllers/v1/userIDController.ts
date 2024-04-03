0719b32ab10f29d7dd17808c11b01aaf8668da90
daniel <wazwazman1@gmail.com>
2024-04-04 00:01:24 +0300

    implemented updateUserPermissions

--- accountService/src/controllers/v1/userIDController.ts
+++ accountService/src/controllers/v1/userIDController.ts
@@ -1,10 +1,29 @@
 import express, { Request, Response } from "express"
+import * as userService from '../../services/userService'
 // IMPLEMENT
 export async function getUserPermissions(req: Request, res: Response) {
-    
+
 }
 
-// IMPLEMENT
 export async function updateUserPermissions(req: Request, res: Response) {
-    
+    const { UUID } = req.params
+    const { permissionOptions } = req.body
+
+    if (typeof (UUID) != "string" || !Array.isArray(permissionOptions)) {
+        res.status(400).json('Bad request')
+        return
+    }
+
+    if (!userService.isPermissionsOptions(permissionOptions)) {
+        res.status(400).json('Bad request')
+    }
+
+    const result: boolean = await userService.updateUserPermissions(UUID, permissionOptions)
+
+    if (result) {
+        res.status(200).json('ok')
+        return
+    }
+    res.status(500).json('internal server error')
+    return
 }
