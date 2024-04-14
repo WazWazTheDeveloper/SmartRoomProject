2c88dd48d010e3df62b54c38fa4722d256904511
daniel <wazwazman1@gmail.com>
2024-04-14 19:18:08 +0300

    added permission verification request

---
+++ controller/src/utils/verifyPermissions.ts
@@ -0,0 +1,27 @@
+import { AxiosResponse } from "axios";
+import { getRequestUUID } from "../middleware/requestID";
+import { loggerGeneral } from "../services/loggerService";
+
+const axios = require('axios').default;
+
+type TResponseData = {
+    hasPermission: boolean
+}
+export async function verifyPermissions(authKey: string, userID: string, type: string, objectid: string, permission: string,) {
+    axios.get(`http://${process.env.ACCOUNT_SERVICE as string}/api/v1/user/${userID}/check-permission?type=${type}&objectid=${objectid}&permission=${permission}`,
+        {
+            headers: {
+                "X-Request-ID": getRequestUUID(),
+                Authorization: authKey
+            }
+        })
+        .then(function (response: AxiosResponse<TResponseData>) {
+            if (response.status != 200) return false
+
+            return response.data.hasPermission
+        }).catch(function (error: any) {
+            // handle error
+            loggerGeneral.error(`failed to fetch permissions verifacation: ${error}`, { uuid: getRequestUUID() })
+            return false;
+        })
+}
