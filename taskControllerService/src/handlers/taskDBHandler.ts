03d6a0dc013d238816f70ee0cf93631301fd697f
WazWaz <wazwazman1@gmail.com>
2024-04-22 13:58:17 +0300

    code

--- taskControllerService/src/handlers/taskDBHandler.ts
+++ taskControllerService/src/handlers/taskDBHandler.ts
@@ -1,5 +1,6 @@
 import * as mongoDB from "mongodb";
-import { stopScheduledTask } from "../services/taskSchedulerService";
+import { addScheduledTask, stopScheduledTask } from "../services/taskSchedulerService";
+import { taskTimeCheckHandler } from "./taskHandler";
 
 /**
  * @description - calls sub functions that handle the device collection changes
@@ -13,9 +14,9 @@ export async function taskDBHandler(changeEvent: mongoDB.ChangeStreamDocument) {
 async function handleTimeCheckChange(changeEvent: mongoDB.ChangeStreamDocument) {
     if (changeEvent.operationType != "update") return;
     if (!changeEvent.updateDescription.updatedFields) return;
-    if (!changeEvent.updateDescription.updatedFields.propertyChecks) return;
+    if (!changeEvent.updateDescription.updatedFields.timeChecks) return;
 
-    if(Array.isArray(changeEvent.updateDescription.updatedFields["propertyChecks"])){
+    if(Array.isArray(changeEvent.updateDescription.updatedFields["timeChecks"])){
         // $pull
         for (let index = 0; index < changeEvent.updateDescription.updatedFields.length; index++) {
             const element = changeEvent.updateDescription.updatedFields[index];
@@ -24,5 +25,11 @@ async function handleTimeCheckChange(changeEvent: mongoDB.ChangeStreamDocument) 
     }
     else {
         // update of added
+        let keys = Object.keys(Array.isArray(changeEvent.updateDescription.updatedFields["timeChecks"]))
+        for (let i = 0; i < keys.length; i++) {
+            const element = changeEvent.updateDescription.updatedFields[keys[i]];
+            stopScheduledTask(element.itemID);
+            addScheduledTask(element.timingData,element.itemID,() => { taskTimeCheckHandler(changeEvent._id as string, element.itemID)})
+        }
     }
 }
