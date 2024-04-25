46df01b790a51d0f6a4cdc2b2ce1ebba3b07d4b9
daniel <wazwazman1@gmail.com>
2024-04-25 17:40:11 +0300

    updated routes

--- authService/src/routes/v1/authRoute.ts
+++ authService/src/routes/v1/authRoute.ts
@@ -4,5 +4,5 @@ import { login, logout, refreshToken } from "../../controllers/v1/authController
 export const loginRouter: express.Router = express.Router();
 
 loginRouter.route("/login").post(login);
-loginRouter.route("/refresh").post(refreshToken);
-loginRouter.route("/logout").post(logout);
+loginRouter.route("/refresh").get(refreshToken);
+loginRouter.route("/logout").delete(logout);
