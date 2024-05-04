e57548e2462781821d0a47544da578ddec96c5cd
daniel <wazwazman1@gmail.com>
2024-05-04 23:42:42 +0300

    added comment

--- deviceControllerService/src/mqttMessages/outgoing/mqttCheckConnectionRequest.ts
+++ deviceControllerService/src/mqttMessages/outgoing/mqttCheckConnectionRequest.ts
@@ -3,6 +3,7 @@ import { publishMessage } from "../../services/mqttClientService";
 /***
  * @description send connection check request to devices
  */
+// TODO: add connection check every 5 seconds using node cron
 export async function sendCheckConnectionRequest() {
     publishMessage(process.env.MQTT_TOPIC_CHECK_CONNECTION_RESPONSE as string,"ping")
 }
