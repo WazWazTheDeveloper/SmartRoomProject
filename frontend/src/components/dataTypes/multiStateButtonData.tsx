c634b6bd7e05811b26aa7657404ee9db48c1d9de
daniel <wazwazman1@gmail.com>
2024-05-11 15:28:18 +0300

    code

--- frontend/src/components/dataTypes/multiStateButtonData.tsx
+++ frontend/src/components/dataTypes/multiStateButtonData.tsx
@@ -1,6 +1,8 @@
 import useAuth from "@/hooks/useAuth"
 import { useState } from "react"
 import * as Icons from '@mui/icons-material'
+import { useMutation } from "react-query"
+import axios from "axios"
 
 interface Props {
     deviceID: string
@@ -21,6 +23,37 @@ export type TStateItem = {
 export default function MultiStateButtonData(props: Props) {
     const auth = useAuth();
     const [state, setState] = useState(props.currentState);
+
+    const updateDeviceMutation = useMutation({
+        mutationFn: async (newState : number) => {
+            const res = await axios.put(`/api/v1/device/${props.deviceID}`, {
+                updateList: [
+                    {
+                        propertyName: "data",
+                        dataPropertyName: "currentState",
+                        newValue: newState,
+                        dataID: props.dataID,
+                        typeID: 2,
+                    }
+                ]
+            }, {
+                headers: {
+                    Authorization: `Bearer ${auth.authToken}`
+                }
+            })
+            if (res.status == 401) {
+                auth.refreshToken()
+            }
+
+            return res.data
+        }
+    })
+
+    function onStateChange(newState : number) {
+        setState(newState)
+        updateDeviceMutation.mutate(newState);
+    }
+
     return (
         <div className='flex justify-start items-center pl-2 pr-2 w-full flex-wrap'>
             {/* <h2 className='text-xl w-full'>{"props.title"}</h2> */}
@@ -30,11 +63,10 @@ export default function MultiStateButtonData(props: Props) {
                     props.stateList.map((item: TStateItem, index: number) => {
                         //@ts-ignore
                         const Icon = Icons[item.icon]
-
                         return (
                             <div key={index} 
                             className={"border-box w-24 h-14 p-2 flex justify-center items-center border border-solid border-neutral-1000 dark:border-darkNeutral-1000 rounded-lg "+ (item.stateValue==state ? "bg-neutral-500 dark:bg-darkNeutral-500" : "bg-neutral-200 dark:bg-darkNeutral-200") }
-                            onClick={()=>{setState(item.stateValue)}}
+                            onClick={()=>{onStateChange(item.stateValue)}}
                             >
                                 {item.isIcon ?
                                     <Icon className='fill-neutral-1000 dark:fill-darkNeutral-1000 dark:border-darkNeutral-300 h-10 w-10' /> :
