cf9aeef4a3095abab8f3547f1d6a34ac106ddd2e
daniel <wazwazman1@gmail.com>
2024-06-03 21:06:45 +0300

    updated mongo-init

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -2,8 +2,8 @@
 rs.initiate({
     _id: "rs0",
     members: [
-        { _id: 0, host: "mongo1" },
-        { _id: 1, host: "mongo2" },
-        { _id: 2, host: "mongo3" }
+        { _id: 0, host: "mongo1", priority: 1},
+        { _id: 1, host: "mongo2", priority: 0.5},
+        { _id: 2, host: "mongo3", priority: 0.5}
     ]
 })
